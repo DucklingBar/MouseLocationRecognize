aed554c0209093b23d738ce600d66d23c8055ffb
Eric Chan <94971888+DucklingBar@users.noreply.github.com>
2024-05-15 18:10:10 +0800

    Add files via upload

---
+++ 鼠标位置记录.py
@@ -0,0 +1,16 @@
+import pyautogui
+# -*- coding: utf-8 -*-
+
+import pyautogui
+import time
+
+try:
+    while True:
+        x,y = pyautogui.position()
+        posi = 'x:' + str(x).rjust(4) + ' y:' + str(y).rjust(4)
+        print('\r',posi,end='')
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    print('已退出！')
+
